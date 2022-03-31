853c8ccffa95930eb45fa4d66242e7b89e055ec7
Leandro Arturi <lea.arturi@gmail.com>
2022-03-31 17:44:03 -0300

    Rutas publicas

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import AuthLayout from './layouts/AuthLayout';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import ForgetPassword from './pages/ForgetPassword';
+import ChangePassword from './pages/ChangePassword';
+import ConfirmAccount from './pages/ConfirmAccount';
 
 function App() {
 
   return (
-    <div className="App">
-      <h1>Hola</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AuthLayout />}>
+          <Route index element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="forget-password" element={<ForgetPassword />} />
+          <Route path="forget-password/:token" element={<ChangePassword />} />
+          <Route path="confirm-account/:token" element={<ConfirmAccount />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
