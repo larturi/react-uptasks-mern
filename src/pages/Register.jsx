e06004c3048d55957acc7faec50d183fff0f54b1
Leandro Arturi <lea.arturi@gmail.com>
2022-04-01 15:57:09 -0300

    Validaciones

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,14 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import Alert from '../components/Alert';
 
 const Register = () => {
+
+  const [nombre, setNombre] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [password2, setPassword2] = useState('');
+  const [alerta, setAlerta] = useState({});
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    if([nombre, email, password, password2].includes('')) {
+      setAlerta({
+        error: true,
+        msg: 'Todos los campos son obligatorios'
+      });
+      return;
+    }
+
+    if(password.length < 6) {
+      setAlerta({
+        error: true,
+        msg: 'El password debe tener al menos 6 caracteres'
+      });
+      return;
+    }
+
+    if(password !== password2) {
+      setAlerta({
+        error: true,
+        msg: 'Los password no coinciden'
+      });
+      return;
+    }
+
+    setAlerta({});
+  }
+
+  const { msg } = alerta;
+
   return (
     <>
       <h1 className='text-sky-600 font-black text-2xl mb-6 uppercase text-center'>
        Crea tu cuenta y administra tus <span className='text-slate-700'>proyectos</span>
       </h1>
 
-      <form className='my-2 bg-white shadow rounded-lg p-10'>
+      {
+        msg && (
+          <Alert alert={alerta} />
+        )
+      }
+
+      <form 
+        className='my-2 bg-white shadow rounded-lg p-10'
+        onSubmit={handleSubmit}
+      >
         <div className='my-2'>
           <label 
             className='uppercase text-gray-600 block font-bold'
@@ -19,6 +68,8 @@ const Register = () => {
             id='nombre'
             placeholder='Nombre'
             className='w-full mt-3 p-3 border rounded-md bg-gray-50'
+            value={nombre}
+            onChange={(e) => setNombre(e.target.value)}
           />
         </div>
 
@@ -32,6 +83,8 @@ const Register = () => {
             id='email'
             placeholder='Email'
             className='w-full mt-3 p-3 border rounded-md bg-gray-50'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
 
@@ -45,6 +98,8 @@ const Register = () => {
             id='password'
             placeholder='Password'
             className='w-full mt-3 p-3 border rounded-md bg-gray-50'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
         </div>
 
@@ -56,8 +111,10 @@ const Register = () => {
           <input 
             type="password"
             id='password2'
-            placeholder='password2'
+            placeholder='Repetir Password'
             className='w-full mt-3 p-3 border rounded-md bg-gray-50'
+            value={password2}
+            onChange={(e) => setPassword2(e.target.value)}
           />
         </div>
 
