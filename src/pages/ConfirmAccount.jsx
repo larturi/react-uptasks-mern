91fb7ea9cd097370d36c182f033f670344198e8c
Leandro Arturi <lea.arturi@gmail.com>
2022-04-04 21:02:54 -0300

    Validacion email en form

--- src/pages/ConfirmAccount.jsx
+++ src/pages/ConfirmAccount.jsx
@@ -1,11 +1,60 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams, Link } from 'react-router-dom';
+import axios from 'axios';
+import Alert from '../components/Alert';
 
 const ConfirmAccount = () => {
+
+  const { token } = useParams();
+
+  const [alerta, setAlerta] = useState({});
+  const [cuentaConfirmada, setCuentaConfirmada] = useState(false);
+
+  useEffect(() => {
+    const confirmarCuenta = async () => {
+      const url = `${import.meta.env.VITE_BACKEND_URL}/api/users/confirm/${token}`
+      try {
+        const res = await axios.get(url);
+        setAlerta({
+          error: false,
+          msg: res.data.msg
+        });
+
+        setCuentaConfirmada(true);
+      } catch (error) {
+        setAlerta({
+          error: true,
+          msg: error.response.data.msg
+        });
+        console.error(error);
+      }
+    }
+
+    confirmarCuenta();
+  }, []);
+
+  const { msg } = alerta;
+
   return (
     <>
       <h1 className='text-sky-600 font-black text-4xl mb-6 uppercase text-center'>
       Confirma tu cuenta y comienza a crear tus <span className='text-slate-700'>proyectos</span>
       </h1>
+
+      <div className='mt-20 md:mt-10 shadow-lg px-5 py-10 rounded bg-white'>
+        {msg && (
+          <Alert alert={alerta} />
+        )}
+
+        {
+          cuentaConfirmada && (
+            <Link 
+              to="/"
+              className='block text-center my-3 text-slate uppercase text-sm'
+            >Iniciar Sesión</Link>
+          )
+        }
+      </div>
     </>
   )
 }
--- src/pages/ForgetPassword.jsx
+++ src/pages/ForgetPassword.jsx
@@ -1,14 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import Alert from '../components/Alert';
 
 const ForgetPassword = () => {
+
+  const [email, setEmail] = useState('');
+  const [alerta, setAlerta] = useState({});
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setAlerta({});
+
+    // Regex to check if email is valid
+    if (!email.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i)) { 
+      setAlerta({
+        error: true,
+        msg: 'Email inválido'
+      });
+      return;
+    } else {
+      setAlerta({});
+    }
+  }
+
+  const { msg } = alerta;
+
   return (
     <>
       <h1 className='text-sky-600 font-black text-3xl mb-6 uppercase text-center'>
        Recuperar mi <span className='text-slate-700'>password</span>
       </h1>
 
-      <form className='my-2 bg-white shadow rounded-lg p-10'>
+      {
+        msg && (
+          <Alert alert={alerta} />
+        )
+      }
+
+      <form 
+        className='my-2 bg-white shadow rounded-lg p-10'
+        onSubmit={handleSubmit}
+      >
 
         <div className='my-3'>
           <label 
@@ -20,6 +53,8 @@ const ForgetPassword = () => {
             id='email'
             placeholder='Email'
             className='w-full mt-3 p-3 border rounded-md bg-gray-50'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
 
